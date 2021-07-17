0cee40dea3be06cee02c4e3645f62a2cbd19858f
Glitch (ludicrous-spectacled-nail) <none>
2021-07-17 12:16:44 +0000

    👨‍👨‍👦🐙 Checkpoint ./controllers/dashboard.js:32891711/685

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,17 +1,20 @@
-"use strict";
+"use strict"
 
 const logger = require("../utils/logger");
 const stationCollection = require("../models/station-store.js");
 const analytics = require("../utils/analytics");
+const conversion = require("./conversion.js");
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
-    const latestReading = {
-      for (let station of stationCollection) {
-        analytics.updateWeather(station);
+    for (let station of stationCollection) {
+      console.log(station);
+      if (station.readings.length > 0) {
+        let lastReading = station.readings(station.readings.length - 1);
+        station.tempF = station.readings.push(conversion.tempF(lastReading.temperature));
+      }
       };
-    };
     const viewData = {
       title: "WeatherTop Dashboard",
       stations: stationCollection,
