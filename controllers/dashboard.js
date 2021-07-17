53c237f64f5b2330165de533358265c669274731
Glitch (ludicrous-spectacled-nail) <none>
2021-07-17 12:27:50 +0000

    🎷💿 Checkpoint ./controllers/dashboard.js:32891711/350

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,13 +8,6 @@ const conversion = require("./conversion.js");
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
-    for (let station of stationCollection) {
-      console.log(station);
-      if (station.readings.length > 0) {
-        let lastReading = station.readings(station.readings.length - 1);
-        station.tempF = station.readings.push(conversion.tempF(lastReading.temperature));
-      }
-      };
     const viewData = {
       title: "WeatherTop Dashboard",
       stations: stationCollection,
